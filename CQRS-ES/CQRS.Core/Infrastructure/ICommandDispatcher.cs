184d87fffcf4a6630962ca93294720008ac165ba
Savio Picanço <saviopbrito@outlook.com>
2024-08-24 15:29:01 -0300

    feat: add ICommandDispatcher

---
+++ CQRS-ES/CQRS.Core/Infrastructure/ICommandDispatcher.cs
@@ -0,0 +1,11 @@
+﻿
+using CQRS.Core.Commands;
+
+namespace CQRS.Core.Infrastructure
+{
+    internal interface ICommandDispatcher
+    {
+        void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand;
+        Task SendAsync(BaseCommand command);
+    }
+}
