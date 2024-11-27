f060f2a00249ffdb2f69b6b88b44bfdef123e977
Savio Picanço <saviopbrito@outlook.com>
2024-11-27 17:00:16 -0300

    feat: add event producer

--- SM-POST/Post.Cmd/Post.Cmd.Api/Program.cs
+++ SM-POST/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,9 +1,11 @@
+using Confluent.Kafka;
 using CQRS.Core.Domain;
 using CQRS.Core.Handlers;
 using CQRS.Core.Infrastructure;
 using Post.Cmd.Api.Commands;
 using Post.Cmd.Domain.Aggregates;
 using Post.Cmd.Infrastruture.Config;
+using Post.Cmd.Infrastruture.Dispatchers;
 using Post.Cmd.Infrastruture.Handlers;
 using Post.Cmd.Infrastruture.Repositories;
 using Post.Cmd.Infrastruture.Stores;
@@ -15,13 +17,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+//env variables injection
 builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
+builder.Services.Configure<ProducerConfig>(builder.Configuration.GetSection(nameof(ProducerConfig)));
+
+// dependency injection
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore,EventStore>();
 builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
 builder.Services.AddScoped<ICommandHandler, CommandHandler>();
 
+
+// dependency injection to commands
+var dispatcher = new CommandDispatcher();
+var commandHandler = builder.Services.BuildServiceProvider().GetRequiredService<ICommandHandler>();
+dispatcher.RegisterHandler<NewPostCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<EditMessageCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<LikePostCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<AddPostCommentCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<EditPostCommentCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<RemovePostCommentCommand>(commandHandler.HandlerAsync);
+dispatcher.RegisterHandler<DeletePostCommand>(commandHandler.HandlerAsync);
+builder.Services.AddSingleton<ICommandDispatcher>(_ => dispatcher);
 
 var app = builder.Build();
 
